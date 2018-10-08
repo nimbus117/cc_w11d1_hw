c45968ccfd556032e0aabe1e8bb8629fe275e92a
nimbus117 <mail@nimbus117.co.uk>
2018-10-08 15:59:34 +0100

    added calculator tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd() {
+        assertEquals(5, calculator.add(2,3));
+    }
+
+    @Test
+    public void canSubtract() {
+        assertEquals(2, calculator.add(5,3));
+    }
+
+    @Test
+    public void canMultiply() {
+        assertEquals(10, calculator.add(2,5));
+    }
+
+    @Test
+    public void canDivide() {
+        assertEquals(5, calculator.add(10,2));
+    }
 }
