d757a078c19bad44e7480c6f46ad542ec2415059
nimbus117 <mail@nimbus117.co.uk>
2018-10-08 16:17:55 +0100

    passed waterbottle tests

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,2 +1,23 @@
 public class WaterBottle {
+    private int volume;
+
+    public WaterBottle() {
+        this.volume = 100;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void drink() {
+        volume = volume - 10;
+    }
+
+    public void empty() {
+        volume = 0;
+    }
+
+    public void fill() {
+        volume = 100;
+    }
 }
